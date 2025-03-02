194a34ccdaa9a9db0d305d3dd80e25c89bf56ae7
Hayden Ringer <hjrrockies@gmail.com>
2025-03-02 13:48:55 -0500

    changes to searchscripts

--- examples/quad_corner_search.py
+++ examples/quad_corner_search.py
@@ -2,38 +2,38 @@ import numpy as np
 from pymps import *
 from joblib import Parallel, delayed
 
-def quad_eig(v1,k,tol=1e-5):
-    v = np.array([0,1,v1[0]+1j*v1[1],1j])
-    A,P = polygon_area(v),1
-    weyl_1 = ((P+np.sqrt(P**2+16*np.pi*A))/(2*A))**2
-    v *= np.sqrt(weyl_1)
+def quad_eig(v1,k,L,H,tol=1e-5):
+    v = np.array([0,L,v1[0]+1j*v1[1],1j*H])
     phis = interior_angles(v)
     alpha = np.pi/phis
     if np.ceil(alpha.max()) <= 100:
         try:
             evp1 = PolygonEVP(v,order=20)
-            eigs = evp1.solve_eigs_ordered(k+1,ppl=30)[0][:k]
+            eigs = evp1.solve_eigs_ordered(k+1)[0][:k]
             out = np.full(k,np.nan)
             out[:len(eigs)] = eigs
-            return weyl_1*out
+            return out
         except:
             pass
     return np.full(k,np.nan)
 
-def run_test(n,k):
-    x,y = np.linspace(0.5,1.5,n),np.linspace(0.5,1.5,n)
+def run_test(n,k,L,H,dx):
+    x,y = np.linspace(L-dx,L+dx,n),np.linspace(H-dx,H+dx,n)
     X,Y = np.meshgrid(x,y,indexing='ij')
     XY = np.vstack((X.flatten(),Y.flatten())).T
 
     res = Parallel(n_jobs=-1,verbose=10)(delayed(quad_eig)(xy,k) for xy in XY)
     eigs = np.array(res)
 
-    np.savez(f"quad_eig_{n}_{k}.npz",X=X,Y=Y,eigs=eigs)
+    np.savez(f"corner_eig_{n}_{k}_{np.rint(L)}_{np.rint(H)}.npz",X=X,Y=Y,eigs=eigs)
 
 
 if __name__=="__main__":
     from sys import argv
     n = int(argv[1])
     k = int(argv[2])
+    L = float(argv[3])
+    H = float(argv[4])
+    dx = float(argv[5])
 
-    run_test(n,k)
+    run_test(n,k,L,H,dx)
