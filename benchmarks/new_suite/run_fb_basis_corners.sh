7acf44e31627c50d09e6e3a23c5e795ec6174fa5
Hayden Ringer <hjrrockies@gmail.com>
2026-03-22 22:34:36 -0400

    sbatch script

--- benchmarks/new_suite/run_fb_basis_corners.sh
+++ benchmarks/new_suite/run_fb_basis_corners.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=fb_basis_corners
 #SBATCH --account=siallocation
 #SBATCH --partition=normal_q
-#SBATCH --time=02:00:00
-#SBATCH --mem=16G
-#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=10
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 
